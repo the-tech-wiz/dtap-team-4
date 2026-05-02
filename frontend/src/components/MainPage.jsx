f438dc2a5a1eaeaeb6135cf29fccdb311287120e
Davidjoneon <horich.david1@gmail.com>
2026-05-02 18:39:30 +0300

    very basic unconnected page

--- frontend/src/components/MainPage.jsx
+++ frontend/src/components/MainPage.jsx
@@ -1,14 +1,12 @@
 import React from 'react'
 import TrackCollection from "./TrackCollection"
 
-const MainPage = () => {
+export default function MainPage() {
     return (
-        <div className="fixed left-0 w-full h-screen bg-gradient-to-t from-[#00635D] to-[#01172F]">
+        <div className="fixed left-0 w-full h-screen custom_bg">
             <div className="p-4">
                 <TrackCollection />
             </div>
         </div>
     )
 }
-
-export default MainPage
--- frontend/src/components/Unconnected.jsx
+++ frontend/src/components/Unconnected.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import { TbMoodSadDizzy } from "react-icons/tb";
 
-const Unconnected = () => {
+export default function Unconnected() {
     return (
-        <div className="fixed left-0 w-full h-screen bg-gradient-to-t from-[#00635D] to-[#01172F]">
-            <div className="p-4">
-                <TbMoodSadDizzy className="iconCenter" />
+        <div className="fixed flex justify-center items-center left-0 w-full h-full custom_bg flex-col">
+            <TbMoodSadDizzy className="centeredIcon" />
+            <div className="flex justify-center items-center bg-white w-60 h-60">
+                No devices detected.
+                I am big sad now :(
             </div>
         </div>
     )
 }
-
-export default Unconnected
