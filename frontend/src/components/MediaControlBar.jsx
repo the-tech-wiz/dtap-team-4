cc2d501055bdfb672cfaa297c367648989f1ec4d
Davidjoneon <horich.david1@gmail.com>
2026-05-14 19:31:28 +0300

    sylized the dissapearing

--- frontend/src/components/MediaControlBar.jsx
+++ frontend/src/components/MediaControlBar.jsx
@@ -7,16 +7,18 @@ import VolumeSlider from "./VolumeSlider";
 // height goes from 0 to 12
 export default function MediaControlBar({ currentTrack, resetter, volume, setVolume, handleVolume }) {
     return (
-        <div className={`transition w-full flex justify-center transition-all duration-300 -translate-y-1 pointer-events-auto ${currentTrack.track == "" ? "opacity-0 h-10" : "opacity-100 h-20"}`}>
-            <div className="w-full py-1 px-5 text-black rounded-lg panel_secondary_background">
-                <div className="flex justify-between">
-                    <div className="p-1">
-                        <span>Now Playing: </span>
-                        <span>{currentTrack.track}</span>
+        <div className={`transition w-full transition-all duration-300 pointer-events-auto ${currentTrack.track == "" ? "opacity-0 h-0 pointer-events-none" : "opacity-100 h-20 pointer-events-auto"}`}>
+            <div className="flex flex-col justify-center w-full gap-2 py-2 px-5 text-black rounded-lg panel_secondary_background">
+                <div className="flex flex-row justify-between w-full p-1">
+                    <div>Now Playing: </div>
+                    <div className="absolute left-1/2 -translate-x-1/2">
+                        {currentTrack.track}
                     </div>
-                    <button onClick={resetter} className="p-1">
-                        <FaPause />
-                    </button>
+                    <div>
+                        <button onClick={resetter}>
+                            <FaPause />
+                        </button>
+                    </div>
                 </div>
                 <VolumeSlider value={volume} setVolume={setVolume} handleVolume={handleVolume} />
             </div>
