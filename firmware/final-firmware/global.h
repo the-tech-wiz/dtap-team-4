0e3f8b02b77753486b43f56675ba90b3b9ee0d8a
lel4 <long.l.le@aalto.fi>
2026-05-22 14:39:37 +0300

    changed to numeric index IDs

--- firmware/final-firmware/global.h
+++ firmware/final-firmware/global.h
@@ -1,13 +1,14 @@
-//project-wide includes to work around weird arduino compilation and ordering
+// project-wide includes to work around weird arduino compilation and ordering
 #include "ESP32MQTTClient.h"
 #include <string>
-using std::string;  //in place of Arduino String project-wide for ESP32 MQTT purposes
+using std::string; // in place of Arduino String project-wide for ESP32 MQTT
+                   // purposes
 
 // default audio file to play (testing/fallback)
-constexpr char DEFAULT_TRACK_ID[]= "Jolly 1";
+constexpr char DEFAULT_TRACK_ID[] = "1";
 
 // status variables (also transmitted via code)
 bool online = false;
 string trackId = DEFAULT_TRACK_ID;
 bool playing = false;
-int volume = 50;  // [0-100] range
+int volume = 50; // [0-100] range
