8f96e7faa96af784600aeeca654709f85fcbdc83
David <horich.david1@gmail.com>
2026-05-22 13:15:18 +0300

    frontend: improved disconnected page

--- frontend/src/components/Unconnected.jsx
+++ frontend/src/components/Unconnected.jsx
@@ -3,9 +3,9 @@ import { TbMoodSadDizzy } from "react-icons/tb";
 
 export default function Unconnected() {
     return (
-        <div className="fixed flex justify-center items-center left-0 w-full h-full custom_bg flex-col">
+        <div className="fixed flex justify-center items-center left-0 w-full h-full flex-col">
             <TbMoodSadDizzy className="centeredIcon" />
-            <div className="flex justify-center items-center bg-white w-60 h-60">
+            <div className="flex justify-center items-center">
                 No devices detected.
                 I am big sad now :(
             </div>
