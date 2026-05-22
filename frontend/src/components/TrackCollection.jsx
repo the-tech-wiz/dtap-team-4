4fa196720e3312f8eccee945ec25d850cb4c6f61
Davidjoneon <horich.david1@gmail.com>
2026-05-22 10:13:08 +0300

    frontend: changed button color to make it look softer

--- frontend/src/components/TrackCollection.jsx
+++ frontend/src/components/TrackCollection.jsx
@@ -12,7 +12,7 @@ const TrackButton = ({ label, onClick }) => {
     return (
         <button
             onClick={onClick}
-            className="bg-blue-500 text-white px-4 py-2 rounded"
+            className="panel_secondary_background text-white px-4 py-2 rounded"
         >
             {label}
         </button>
